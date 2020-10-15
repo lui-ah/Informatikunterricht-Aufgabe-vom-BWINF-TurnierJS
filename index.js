fd52eac793e3f3d54544a9a06fce868030d2e062
lui-ah <luisahrens310@gmail.com>
2020-10-15 22:43:31 +0200

    initChart nimmt jetzt die labels als Argumente ist ein wenig schöner so.

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 // Import stylesheets
-import './style.css';
-import {duel, shuffle, groupDuel, chunk, addDataset, initChart, updateLabels } from './method'
-import {turnier2 } from './map'
+import "./style.css";
+import {
+  duel,
+  shuffle,
+  groupDuel,
+  chunk,
+  addDataset,
+  initChart,
+  updateLabels
+} from "./method";
+import { turnier2 } from "./map";
 
 // Write TypeScript code!
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById("app");
 
-let DATA = turnier2.split('\n');
+let DATA = turnier2.split("\n");
 // const ANZAHLSPIELER = DATA[0];
 DATA.shift();
-const SPIELER = DATA; 
+const SPIELER = DATA;
 
 function turnier(players) {
   const shuffledPLayers = shuffle(players);
@@ -19,43 +27,41 @@ function turnier(players) {
   return winner;
 }
 
-
-
 function addWin(players, func) {
-  const player = players[players.indexOf(func(players))]
+  const player = players[players.indexOf(func(players))];
   player[func.name] = player[func.name] + 1 || 1;
 }
 
-const times = x => f => {if (x > 0) {f(),times(x - 1)(f)}}
+const times = x => f => {
+  if (x > 0) {
+    f(), times(x - 1)(f);
+  }
+};
 
 function app(samples = 2000) {
   console.clear();
   const playersValueArray = SPIELER.map(spieler => parseInt(spieler));
-  const pool = playersValueArray.reduce((a,b) => a + b);
+  const pool = playersValueArray.reduce((a, b) => a + b);
 
   const players = playersValueArray.map((player, index) => {
     return {
-      name: 'player' + (index + 1),
+      name: "player" + (index + 1),
       value: player,
-      ProWins: Math.round((((player / pool) * samples) + Number.EPSILON) * 100) / 100      
+      ProWins:
+        Math.round(((player / pool) * samples + Number.EPSILON) * 100) / 100
     };
   });
   // Players Reinfolge egal weil die spieler im Turnier ge shuffeld werden.
 
+  const labels = [...players].map(player => `${player.name} p${player.value}`);
+  const chart = initChart(labels);
 
-  const chart = initChart();
-
-  const labels = [...players].map(player => `${player.name} p${player.value}`)  
-  chart.data.labels = labels;
-  
   const tournamentTypes = [turnier];
 
-  times(samples)(() => tournamentTypes.forEach(type => addWin(players, type)))
-  tournamentTypes.forEach(type => addDataset(chart, players, type.name))
+  times(samples)(() => tournamentTypes.forEach(type => addWin(players, type)));
+  tournamentTypes.forEach(type => addDataset(chart, players, type.name));
   // Das ist jetzt so hässlich in einer Zeile damit man das besser skalieren kann
-  addDataset(chart, players, 'ProWins')
-
+  addDataset(chart, players, "ProWins");
 }
-
 
 app(1000);
--- method.js
+++ method.js
@@ -1,37 +1,34 @@
-import {Chart} from 'chart.js'
-import 'chartjs-plugin-colorschemes/src/plugins/plugin.colorschemes';
- 
-import { Aspect6 } from 'chartjs-plugin-colorschemes/src/colorschemes/colorschemes.office';
+import { Chart } from "chart.js";
+import "chartjs-plugin-colorschemes/src/plugins/plugin.colorschemes";
 
+import { Aspect6 } from "chartjs-plugin-colorschemes/src/colorschemes/colorschemes.office";
 
 export function duel(player1, player2) {
-  
   let pool = player1 + player2;
 
-  let random = Math.random() // zufällige zahl zwischen 0 und 1
+  let random = Math.random(); // zufällige zahl zwischen 0 und 1
   // player1 / pool sollte kleiner als 1 sein.
   return player1 / pool > random;
 }
 
 export function chunk(array, chunkSize) {
-    var R = [];
-    for (var i = 0; i < array.length; i += chunkSize)
-      R.push(array.slice(i, i + chunkSize));
-    return R;
+  var R = [];
+  for (var i = 0; i < array.length; i += chunkSize)
+    R.push(array.slice(i, i + chunkSize));
+  return R;
 }
-
 
 /**
  * Shuffles array in place. ES6 version
  * @param {Array} a items An array containing the items.
  */
 export function shuffle(c) {
-    let a = [...c];
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
+  let a = [...c];
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
 }
 
 export function groupDuel(groups, round = 1) {
@@ -43,53 +40,50 @@ export function groupDuel(groups, round = 1) {
     let player2 = group[1];
     let winner = duel(player1.value, player2.value) ? player1 : player2;
     winners.push(winner);
-  })
+  });
   // console.log(winners, 'round', round, 'winner')
   if (winners.length === 1) {
     return winners[0];
   } else {
-    let chunkedWinners = chunk(winners, 2)
-    return groupDuel(chunkedWinners, round + 1)
+    let chunkedWinners = chunk(winners, 2);
+    return groupDuel(chunkedWinners, round + 1);
   }
 }
 
-export function initChart() {
-  let labels = [];
-  let data = [];
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'bar',
+export function initChart(labels = []) {
+  const ctx = document.getElementById("myChart").getContext("2d");
+  const myChart = new Chart(ctx, {
+    type: "bar",
     data: {
-        labels: labels,
-        datasets: []
+      labels: labels,
+      datasets: []
     },
     options: {
       plugins: {
-            colorschemes: {
-                scheme: Aspect6
-            }
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
+        colorschemes: {
+          scheme: Aspect6
         }
+      },
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      }
     }
   });
-  return myChart
+  return myChart;
 }
-
-
 
 export function addDataset(chart, players, property) {
   let data = [];
 
-
   players.forEach((winner, index) => {
-    data.push(winner[property])
-  })
+    data.push(winner[property]);
+  });
   chart.data.datasets.push({
     data,
     label: property
@@ -98,7 +92,5 @@ export function addDataset(chart, players, property) {
   chart.update();
   let indexOfDataSet = chart.data.datasets.length - 1;
 
-
   return indexOfDataSet;
-  
 }
