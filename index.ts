c41f9853f22ce0daa9815c3eb3f99e58c248ff0e
lui-ah <luisahrens310@gmail.com>
2020-10-28 22:50:52 +0100

    Slider hinzugefügt

--- index.ts
+++ index.ts
@@ -14,22 +14,34 @@ import { turnier, turnierX5 } from "./turniertypen";
 const tournamentTypes = [turnier, turnierX5];
 
 import { turnier1, turnier2 } from "./map";
+import { fromEvent, combineLatest, merge } from "rxjs";
+import { map, startWith } from "rxjs/operators";
 const textDateien = [turnier1, turnier2];
 
 const initialize = () => {
-  textDateien.forEach((DATA, index) => {
+  let samplesSlider = document.getElementById("samples") as HTMLInputElement;
+  let slider = fromEvent(samplesSlider, "change").pipe(
+    startWith(null),
+    map(() => parseInt(samplesSlider.value))
+  );
+
+  let obs = textDateien.map((DATA, index) => {
     let mapName = "Turnier#" + index;
     let mapOption = document.createElement("div");
     mapOption.classList.add("mapOption");
     mapOption.innerHTML = mapName;
-    mapOption.addEventListener("click", function() {
-      interfaceDiv
-        .querySelectorAll("div")
-        .forEach(entry => entry.classList.remove("active"));
-      mapOption.classList.add("active");
-      app(100000, DATA);
-    });
     interfaceDiv.appendChild(mapOption);
+
+    return fromEvent(mapOption, "click").pipe(map(() => DATA));
+  });
+  let maps = merge(...obs);
+  let options = combineLatest([slider, maps]);
+  options.subscribe(event => {
+    // interfaceDiv
+    // .querySelectorAll("div")
+    // .forEach(entry => entry.classList.remove("active"));
+    // mapOption.classList.add("active");
+    app(event[0], event[1]);
   });
 };
 
